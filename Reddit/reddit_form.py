ff430d46ddee10df2f14a7c105f0ca585f791c60
Eddy Gergi <imeddygergi@gmail.com>
2024-04-04 12:50:59 +0300

    zabat reddit login bas baad fi kam sha8le

--- Reddit/reddit_form.py
+++ Reddit/reddit_form.py
@@ -8,6 +8,24 @@ import time
 driver = webdriver.Firefox()
 driver.get("https://www.reddit.com/report")
 
+email = driver.find_element(By.XPATH, '/html/body/shreddit-app/shreddit-overlay-display/span[4]/input')
+email.send_keys("michael.me2@hotmail.com")
+
+time.sleep(12)
+
+password = driver.find_element(By.XPATH, '//*[@id="login-password"]')
+password.send_keys("random1$A" + Keys.ENTER)
+
+#time.sleep(2)
+
+#WebDriverWait(driver, 12).until(
+#    EC.presence_of_element_located((By.XPATH, '/html/body/shreddit-app/shreddit-overlay-display//shreddit-signup-drawer//shreddit-drawer/div/shreddit-async-loader/div/shreddit-slotter//span/shreddit-async-loader/auth-flow-login/faceplate-form/faceplate-tabpanel/auth-flow-modal[1]/div[2]'))
+#)
+#login = driver.find_element(By.XPATH, '/html/body/shreddit-app/shreddit-overlay-display//shreddit-signup-drawer//shreddit-drawer/div/shreddit-async-loader/div/shreddit-slotter//span/shreddit-async-loader/auth-flow-login/faceplate-form/faceplate-tabpanel/auth-flow-modal[1]/div[2]')
+#login.click()
+
+time.sleep(15)
+
 other_issues = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div/div/div/div/div/div[2]/div/div[1]')
 other_issues.click()
 
@@ -48,7 +66,7 @@ report_type.click()
 
 time.sleep(3)
 
-investigations = driver.find_element(By.XPATH, '/html/body/div[3]/div/main/div[2]/div/div/div/div[8]/div/form/div[3]/a')
+investigations = driver.find_element(By.XPATH, '/html/body/div[3]/div/main/div[2]/div/div/div/div[8]/div/form/div[5]')
 investigations.click()
 
 time.sleep(3)
