ca5d7ad258d396f8734bff7d3e8a4d7bbc8b816d
Eddy Gergi <imeddygergi@gmail.com>
2024-04-04 21:38:47 +0300

    added links

--- Reddit/reddit_form.py
+++ Reddit/reddit_form.py
@@ -17,14 +17,6 @@ time.sleep(12)
 password = driver.find_element(By.XPATH, '//*[@id="login-password"]')
 password.send_keys("random1$A" + Keys.ENTER)
 
-time.sleep(2)
-
-WebDriverWait(driver, 12).until(
-    EC.presence_of_element_located((By.XPATH, '/html/body/shreddit-app/shreddit-overlay-display//shreddit-signup-drawer//shreddit-drawer/div/shreddit-async-loader/div/shreddit-slotter//span/shreddit-async-loader/auth-flow-login/faceplate-form/faceplate-tabpanel/auth-flow-modal[1]/div[2]'))
-)
-login = driver.find_element(By.XPATH, '/html/body/shreddit-app/shreddit-overlay-display//shreddit-signup-drawer//shreddit-drawer/div/shreddit-async-loader/div/shreddit-slotter//span/shreddit-async-loader/auth-flow-login/faceplate-form/faceplate-tabpanel/auth-flow-modal[1]/div[2]')
-login.click()
-
 time.sleep(15)
 
 other_issues = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div/div/div/div/div/div[2]/div/div[1]')
@@ -63,27 +55,22 @@ email_address.send_keys("marcelle.haddad@outook.com")
 time.sleep(4)
 
 report_type = driver.find_element(By.XPATH, '/html/body/div[3]/div/main/div[2]/div/div/div/div[8]/div/form/div[3]/a')
-report_type.click()
+report_type.send_keys(Keys.ENTER + Keys.DOWN + Keys.ENTER)
 
 time.sleep(3)
 
-investigations = driver.find_element(By.XPATH, '/html/body/div[3]/div/main/div[2]/div/div/div/div[8]/div/form/div[5]')
-investigations.click()
-
-time.sleep(3)
-
-sub_inquiry = driver.find_element(By.XPATH, '/html/body/div[3]/div/main/div[2]/div/div/div/div[8]/div/form/div[18]/input')
-sub_inquiry.send_keys(" ")
+sub_inquiry = driver.find_element(By.XPATH, '//*[@id="request_subject"]')
+sub_inquiry.send_keys("xxx")
 
 time.sleep(2)
 
-details_inquiry = driver.find_element(By.XPATH, '/html/body/p')
-details_inquiry.send_keys(" ")
+#details_inquiry = driver.find_element(By.XPATH, '//*[@id="request_description_ifr"]')
+#details_inquiry.send_keys("xxx")
 
 time.sleep(2)
 
-checkbox = driver.find_element(By.XPATH, '/html/body/div[3]/div/main/div[2]/div/div/div/div[8]/div/form/div[29]/input[2]')
-checkbox.click()
+#checkbox = driver.find_element(By.XPATH, '/html/body/div[3]/div/main/div[2]/div/div/div/div[8]/div/form/div[29]/input[2]')
+#checkbox.click()
 
 time.sleep(2)
 
